cb08ca21f6e8223724a6c806301931cac90d2e08
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2021-03-08 13:05:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
-variable "organization" {}
-variable "terraform_version" {}
-variable "vcs_oauth_token_id" {}
-
+variable "organization" {} ## Your TFC Organization
+variable "terraform_version" {} ## TFC version you would like to use(example: 0.12.26)
+variable "vcs_oauth_token_id" {} ## Your VCS Oauth token
+variable "vcs_repoorg" {}  ## Your VCS organization, where `different-repo` get forked
 
 module "my_new_workspace" {
 
   source = "./modules/workspace"
 
   organization = var.organization
+  
+  vcs_repoorg = var.vcs_repoorg
 
   vcs_reponame = "different-repo"
 
